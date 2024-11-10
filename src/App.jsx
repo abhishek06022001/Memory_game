79fde49798dfb6939f055ade15a84dee5b633ad9
Abhishek <satheabhishek1@gmail.com>
2024-11-10 23:55:36 +0530

    little workings left

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, } from 'react';
+
 import axios, { all } from 'axios';
+import { BounceLoader } from 'react-spinners';
+
+
 
 function App() {
   const access_token = "7927a62e31cfa459b5b3dd16fc5467d7";
@@ -14,16 +18,12 @@ function App() {
       try {
         let fetchedCards = [];
         let i = 1;
-        while (i < 33) {
-          const response = axios.get(`/api/${access_token}/${i}`);
-          fetchedCards.push(response);
-          i++;
-        }
-        const response = await all(fetchedCards);
-        setCards(response);
+        const response =await axios.get(`https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json`);
+        console.log(response);
+        
+        const cards = response.data.slice(0, 45);
+        setCards(cards);
         setLoading(false);
-        console.log("response is", response);
-
       } catch (error) {
         console.error('Error fetching cards:', error);
       }
@@ -32,26 +32,38 @@ function App() {
   }, []);
 
   return (
-    <div className='min-h-screen p-5 bg-gradient-to-b from-indigo-500 via-purple-500 to-pink-500'>
+    <div className='min-h-screen p-5 bg-gradient-to-b from-indigo-500 flex flex-col via-purple-500 to-pink-500'>
       <div className='flex justify-between  px-5  items-center text-[#070206] py-3'>
         <div className='md:text-6xl sm:text-md font-bold'>
           The Memory Game
+        
         </div>
         <div className='md:text-lg sm:text-xs font-semibold'>
           <div>CURRENT SCORE: {score.current_score}</div>
           <div>MAX SCORE: {score.max_score}</div>
         </div>
       </div>
-      <div className='my-4 gap-5  sm:flex sm:flex-wrap  grid grid-cols-2'>
-        {cards && cards.map((card) => {
-          return <div className='bg-white   sm:w-auto  mx-auto px-2 py-1 rounded-md 
+      {cards ?
+        <div className='my-4 gap-5  sm:flex sm:flex-wrap  grid grid-cols-2'>
+
+          {cards.map((card) => {
+            return <div className='bg-white 
+            shadow-lg shadow-white hover:shadow-2xl hover:shadow-black
+            sm:w-auto  mx-auto px-2 py-1 rounded-md 
+           transition-shadow duration-300 ease-in
           flex flex-col justify-center
-          ' key={card.data.connections.id} >
-            <img className='  sm:h-32 md:h-44 rounded-md ' src={card.data.image.url} alt="" />
-            <div className='text-center font-bold'>{card.data.name}</div>
-          </div>
-        })}
-      </div>
+          ' key={card.id} >
+              <img className='  sm:h-32 md:h-44 rounded-md ' src={card.images.sm} alt="" />
+              <div className='text-center font-bold'>{card.name}</div>
+            </div>
+          })}
+        </div>
+        :
+        <div className='flex-1 flex justify-center items-center' >
+          <BounceLoader />
+        </div>
+
+      }
     </div>
   );
 }
