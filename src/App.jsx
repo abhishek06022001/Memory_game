63db7d44557c21cea5b18339cd60fcaf3c8750b5
Abhishek <satheabhishek1@gmail.com>
2024-11-11 20:17:22 +0530

    Completed the memory game

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,8 @@ import React, { useEffect, useState, } from 'react';
 
 import axios, { all } from 'axios';
 import { BounceLoader } from 'react-spinners';
-
-
-
 function App() {
-  const [currId, setCurrId] = useState(null);
+  const [currId, setCurrId] = useState([]);
   const [cards, setCards] = useState(null);
   const [loading, setLoading] = useState(true);
   const [score, setScore] = useState({
@@ -14,13 +11,12 @@ function App() {
   });
   function shuffle(e) {
     let someId = e.currentTarget.getAttribute('data-id');
-    // alert(someId);
-    
-    if (currId == someId) {
-      setScore({ ...score, max_score: score.current_score, current_score: 0 })
+    if (currId.includes(someId)) {
+      setScore({ ...score, max_score: (score.max_score > score.current_score) ? score.max_score : score.current_score, current_score: 0 })
     } else {
       setScore({ ...score, current_score: score.current_score + 1 });
     }
+    setCurrId([...currId, someId]);
     let shuffled_cards = [];
     let usedIndex = [];
     while (shuffled_cards.length < cards.length) {
@@ -31,20 +27,18 @@ function App() {
       }
     }
     setCards(shuffled_cards);
-
   }
-
   useEffect(() => {
     async function fetchCards() {
       try {
         let fetchedCards = [];
         let i = 1;
         const response = await axios.get(`https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json`);
-        console.log(response);
-
         const response_cards = response.data.slice(0, 27);
-        setCards(response_cards);
-        setLoading(false);
+        setTimeout(() => {
+          setCards(response_cards);
+          setLoading(false);
+        }, 1500);
       } catch (error) {
         console.error('Error fetching cards:', error);
       }
@@ -57,7 +51,6 @@ function App() {
       <div className='flex justify-between  px-5  items-center text-[#070206] py-3'>
         <div className='md:text-5xl  text-xl font-extrabold'>
           THE MEMORY GAME
-
         </div>
         <div className='md:text-md text-sm font-semibold text-black bg-white rounded-sm  p-2'>
           <div className='flex justify-between gap-5'><div>current score</div> <div>{score.current_score}</div></div>
@@ -68,7 +61,6 @@ function App() {
         <div className='flex-1 flex justify-center items-center' >
           <BounceLoader />
         </div>
-
         :
         <div className='my-4 gap-5  sm:flex sm:flex-wrap  grid grid-cols-2'>
 
@@ -79,7 +71,7 @@ function App() {
            transition-shadow duration-300 ease-in
           flex flex-col justify-center
           '
-              onClick={(e) => { shuffle(e), setCurrId(card.id)}}
+              onClick={(e) => shuffle(e)}
               data-id={card.id}
               key={card.id} >
               <img className='  sm:h-32 md:h-44 rounded-md ' src={card.images.sm}
